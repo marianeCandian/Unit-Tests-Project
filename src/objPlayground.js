a1daa0a6a89fd6b402f924688e2b91e6f1b0dc8f
Mariane Candian <mariane.candian@gmail.com>
2022-08-17 20:28:36 -0300

    Requisito 8

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+ const object = {
+    sum: (number1 + number2),
+    mult: (number1 * number2),
+    div: Math.trunc(number1 / number2),
+    sub: (number1 - number2),
+  };
+  return object;
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  let array = [];
+  if (type === 'keys') {
+    array = Object.keys(object);
+  } else if (type === 'values') {
+    array = Object.values(object);
+  } else if (type === 'entries') {
+    array = Object.entries(object);
+  }
+  return array;
+};
 
 module.exports = { calculator, arrayGenerator };
